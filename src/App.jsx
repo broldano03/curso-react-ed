9a2b3f71cd6099c0c3802244b823be5218a9c3a5
Bruno Roldan <broldano@unitru.edu.pe>
2024-10-17 00:13:28 -0500

    api connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from "react"
+
 function App() {
+
+  useEffect(()=> {
+    fetch("https://api.coincap.io/v2/assets")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch(()=>{
+        console.error("La petición falló")
+      })
+  },[])
+
   return (
-    <h1>Vite + React</h1>
+    <h1>Lista de criptomonedas</h1>
   )
 }
 
